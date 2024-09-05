8d39b534b50b9cb01ee72b60e65c54bdc463e7af
omer <krkanomer@yandex.com>
2024-09-05 13:57:15 +0300

    module name changed

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ import {
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const model = new LlamaModel({
-    // modelPath: path.join(__dirname, "models", "llama-2-7b-chat.Q3_K_S.gguf")
-    modelPath: path.join(__dirname, "models", "Mistral-NeMo-Minitron-8B-Base.IQ3_M.gguf")
+    modelPath: path.join(__dirname, "models", "Mistral-NeMo-Minitron-8B-Base.Q2_K.gguf") // For server
+    // modelPath: path.join(__dirname, "models", "Mistral-NeMo-Minitron-8B-Base.IQ3_M.gguf")
 });
 const context = new LlamaContext({model});
 const session = new LlamaChatSession({context});
